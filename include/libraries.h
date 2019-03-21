7f315685b59ebc7e4866c4b91d19c0122914240d
Igorello <sidneff.igor@ya.ru>
2019-03-21 18:49:08 +0500

    Adding "OneTimer" library

--- include/libraries.h
+++ include/libraries.h
@@ -2,7 +2,9 @@
 #include "config.h" //config file for the project
 
 #include <Keypad.h>
+#include <TimerOne.h>
 
 void libInit() {
     Keypad key = Keypad( makeKeymap(KEY_KEYS), KEY_ROWS_PINS, KEY_COLS_PINS, KEY_ROWS, KEY_COLS);
+    
 }
