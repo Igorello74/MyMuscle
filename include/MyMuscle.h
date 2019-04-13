c48e750ed1dd9bd93fbc4cdc5432654adbb9bd17
Igorello <sidneff.igor@ya.ru>
2019-04-13 12:46:17 +0500

    Simplification according to lite

--- include/MyMuscle.h
+++ include/MyMuscle.h
@@ -1,11 +1,7 @@
 //File to include libraries and other header file into project
 #include "config.h" //config file for the project
 
-#include <Keypad.h>
-Keypad keypad = Keypad(makeKeymap(KEYPAD_KEYS), KEYPAD_ROWS_PINS, KEYPAD_COLS_PINS, KEYPAD_ROWS, KEYPAD_COLS);
-
 #include <TimerOne.h>
-#include <Wire.h>
 
 void libInit() {
     #ifdef DEBUG
