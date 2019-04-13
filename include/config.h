9884c9620a103e5d16ebbe5bd26f06ed247ecbcc
Igorello-mobile <sidneff.igor@ya.ru>
2019-04-13 21:25:18 +0500

    Adding stick parameters to config.h

---
+++ include/config.h
@@ -0,0 +1,8 @@
+//sticks settings
+#define STICK1_PIN //the pin which the stick is connected to
+#define STICK1_MAXVAL //the maximal value that might be on the stick
+#define STICK1_MINVAL //the minimal value that might be on the stick
+
+#define STICK2_PIN //the same as in the stick 1 ^^^
+#define STICK2_MAXVAL
+#define STICK2_MINVAL
