97b22bbc4a8e98620e57a2aed5ea65557e80fe0a
Igorello <sidneff.igor@ya.ru>
2019-04-13 12:46:17 +0500

    Simplification according to lite

--- src/main.cpp
+++ src/main.cpp
@@ -7,25 +7,9 @@
 void setup() {
   // put your setup code here, to run once:
 	libInit();
-	keypad.addEventListener(NULL);
-
 }
 
 void loop() {
     // put your main code here, to run repeatedly:
 
-}
-
-void keyHandler(KeypadEvent key) {
-	switch (keypad.getState())	{
-		case PRESSED:
-			if (key >= 48 and key <= 57) { // range 48-57 is digits in ASCII table
-				debug("I saw a number!");
-			}
-			break;
-	
-		default:
-			NULL;
-			break;
-	}
 }
