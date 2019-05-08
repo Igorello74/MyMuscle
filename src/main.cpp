46809284f596bc45d1a976eebcdf40afd2c873b9
Igorello-work <sidneff.igor@ya.ru>
2019-05-08 19:51:12 +0500

    partially add demo mode (NOT FINISHED)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
-#define DEBUG //debug on/off, comment to disable
+#define DEBUG	//debug on/off, comment to disable debug
+#define DEMO	//means that it doesn't care of potentiometers and uses a generator (increases parameters and then resets)
 
 #include <Arduino.h>
 #include "MyMuscle.h"
 
+#ifndef DEMO
 Stick stick[2]={
 	Stick(STICK1_PIN,STICK1_MINVAL,STICK1_MAXVAL),
 	Stick(STICK2_PIN,STICK2_MINVAL,STICK2_MAXVAL)
 };
+#endif
 
 void setup() {
 	libInit(); //libraries initialization
@@ -16,6 +19,7 @@ void setup() {
 }
 
 void loop() {
+	//----------------------------------------------------------------
 	static byte lastFreq = 0;
 	static uint16_t lastDuty = 0;
 	
